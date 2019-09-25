9f3f22d57e19733829da61a7865c24af23c16927
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-25 14:57:14 -0400

    debugging radio button mayhem...
    grrrr

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -1,21 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   FormControl,
   InputLabel,
   Input,
   FormHelperText,
+  Radio,
 } from '@material-ui/core';
 import styles from './AddEvent.module.scss';
 
 const AddEvent = props => {
+  const [allDay, setAllDay] = useState(false);
+  const [test, setTest] = useState(false)
+  const changeAllDay = () => {
+    setAllDay(!allDay)
+    console.log(allDay)
+  };
+  const testing = () => {
+    setTest(!test)
+    console.log(test)
+  }
   return (
     <div className={styles.formContainer}>
       <FormControl className={styles.form}>
-        <InputLabel htmlFor="title" className={styles.label}>Event Title</InputLabel>
-        <Input id="title" aria-describedby="my-helper-ext" className={styles.input} />
+        <InputLabel htmlFor="title" className={styles.label}>
+          Event Title
+        </InputLabel>
+        <Input
+          id="title"
+          aria-describedby="my-helper-ext"
+          className={styles.input}
+        />
         <FormHelperText id="my-helper-text" className={styles.helperText}>
-          We'll never share your email.
+          What would you like to call your event?
         </FormHelperText>
+      </FormControl>
+      <FormControl className={styles.form}>
+        <InputLabel htmlFor="start" className={styles.label}>
+          Start Time
+        </InputLabel>
+        <Input
+          id="start"
+          aria-describedby="enter start time"
+          className={styles.input}
+        />
+        <FormHelperText id="enter start time" className={styles.helperText}>
+          What time does the event start?
+        </FormHelperText>
+      </FormControl>
+      <FormControl className={styles.form}>
+        <InputLabel htmlFor="end" className={styles.label}>
+          End Time
+        </InputLabel>
+        <Input
+          id="end"
+          aria-describedby="enter end time"
+          className={styles.input}
+        />
+        <FormHelperText id="enter end time" className={styles.helperText}>
+          What time does the event end?
+        </FormHelperText>
+      </FormControl>
+      <FormControl className={styles.form}>
+        <InputLabel htmlFor="allDay" className={styles.label}>
+          All Day Event?
+        </InputLabel>
+        <Radio
+          onChange={changeAllDay}
+          value="true"
+          color="default"
+          name="allDay"
+          inputProps={{'aria-label': 'true'}}
+        />
       </FormControl>
     </div>
   );
