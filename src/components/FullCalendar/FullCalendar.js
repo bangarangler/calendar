c887ffa4968e6f0fe812c83ae779fb2f1bfc6611
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-26 14:08:55 -0400

    Using custom-fullcalendar scss to overide styling...
    this way if they update in the future we will still have access

--- src/components/FullCalendar/FullCalendar.js
+++ src/components/FullCalendar/FullCalendar.js
@@ -11,43 +11,7 @@ import SpringModal from '../Modal/Modal.js';
 import './main.scss';
 
 const Cal = props => {
-  const eventObj = [
-    {
-      id: 0,
-      title: 'Test Title',
-      allDay: false,
-      start: new Date(),
-      end: new Date(moment().add(1, 'days')),
-    },
-    {
-      id: 1,
-      title: 'All Day Event',
-      allDay: true,
-      start: new Date(moment().add(1, 'days')),
-      end: new Date(moment().add(1, 'days')),
-    },
-    {
-      id: 2,
-      title: 'Meeting with Bob',
-      allDay: false,
-      start: new Date(moment().add(2, 'days')),
-      end: new Date(moment().add(2, 'days', 1, 'hours')),
-    },
-    {
-      id: 3,
-      title: 'Video chat with Molly',
-      allDay: false,
-      start: new Date(),
-      end: new Date(moment().add(3, 'hours')),
-    },
-    {
-      id: 4,
-      title: 'Put it here',
-      allDay: false,
-      start: new Date(moment().subtract(6, 'days')),
-      end: new Date(moment().subtract(6, 'days')),
-    },
-  ];
+
   const { events, setEvents } = useContext(EventContext)
   const { Loader } = useContext(SpinnerContext)
   const [open, setOpen] = React.useState(false);
@@ -66,9 +30,9 @@ const Cal = props => {
   useEffect(() => {
   run()
   }, [])
+
   const handleDateClick = arg => {
     console.log(arg);
-    //setShowForm(true)
     setOpen(true);
   };
 
@@ -82,6 +46,11 @@ const Cal = props => {
         selectable="true"
         //unselectAuto="true"
         events={events}
+        eventBackgroundColor='#13348b'
+        eventColor="#13348b"
+        eventBorderColor="white"
+        eventTextColor="#eee"
+        eventTimeFormat={{hour: 'numeric', minute: '2-digit', meridiem: 'short'}}
       /> : Loader()}
       {open && (
         <SpringModal
